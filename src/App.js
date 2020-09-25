71e4e1460d8434330eb9e73ba694186b0328d415
Junhee <59816563+wjunhee102@users.noreply.github.com>
2020-09-25 23:28:14 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+
+function HeaderBox() {
+  return (
+    <header>
+      <nav>
+        <div className="inner">
+          <h1>안녕하세요</h1>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <HeaderBox />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
